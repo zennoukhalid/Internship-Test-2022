bda51d8cd4a4fb708fde584e98e7199079e8c263
zennoukhalid <khalid1zennou@gmail.com>
2022-01-11 20:03:28 +0100

    my app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import List from './comps/list.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <List></List>
+    </div>
+  );
+}
+
+export default App;
